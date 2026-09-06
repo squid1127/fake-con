4515499bef57c646e54377afa125ded601f546d8
squid1127 <120748767+squid1127@users.noreply.github.com>
2026-09-06 11:00:55 -0700

    chore: burn everything

--- src/fake_con/__init__.py
+++
@@ -1,5 +0,0 @@
-from .api import FakeCon, connect
-from .joycontrol.controller import Controller
-from .macros import MacroController
-
-__all__ = ["Controller", "FakeCon", "MacroController", "connect"]
